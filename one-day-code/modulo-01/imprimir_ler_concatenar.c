4bac6ed21702bc54225868fbed17748a41862252
nely de faria <nelly.nf54@gmail.com>
2021-07-02 15:58:04 -0300

    Adiciona arquivo imprimir_ler_concatenar.c

---
+++ one-day-code/modulo-01/imprimir_ler_concatenar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b;
+
+    printf("%d", a);
+
+    printf("\n");
+
+    printf("O valor da var a é: %d", a);
+
+    a = 15;
+
+    printf("\nO valor da var a é: %d", a);
+
+    printf("\nInforme um valor para b: \n");
+    scanf("%d", &b);
+
+    printf("\nO valor da var b é: %d", b);
+
+    printf("\n");
+
+    return 0;
+}
