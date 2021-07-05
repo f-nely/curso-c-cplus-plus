ef6c9a54d48161f8db9fb52b0713a12f107ae0fc
nely de faria <nelly.nf54@gmail.com>
2021-07-05 15:14:55 -0300

    Adiciona arquivo condicionais_com_char.c

---
+++ one-day-code/modulo-02/condicionais_com_char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    char letra = 'x';
+
+    if (letra == 'x') {
+        printf("A letra é x.\n");
+    }
+
+    printf("Código da letra x = %d\n", letra);
+}
+
