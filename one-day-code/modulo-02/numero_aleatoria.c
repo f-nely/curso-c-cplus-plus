4a6720c6438159fdd72df4d7f04c8c613be95232
nely de faria <nelly.nf54@gmail.com>
2021-07-05 15:20:26 -0300

    Adicionando comentário

--- one-day-code/modulo-02/numero_aleatoria.c
+++ one-day-code/modulo-02/numero_aleatoria.c
@@ -7,7 +7,7 @@ int main()
     srand((unsigned) time(NULL));
 
     int aleatoria = rand() % 3;
-    int aleatoriaSegundo = (rand() % 5) + 1;
+    int aleatoriaSegundo = (rand() % 5) + 1; // numero entre 1 e 5
 
     printf("%d\n", aleatoriaSegundo);
 
