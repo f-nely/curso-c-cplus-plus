e5b2224241215a6ca49cc392641d988964a8499f
nely de faria <nelly.nf54@gmail.com>
2021-07-05 15:14:16 -0300

    Adiciona arquivo condicionais_booleana.c

---
+++ one-day-code/modulo-02/condicionais_booleana.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main()
+{
+    bool a = true, b = false;
+
+    if (a) {
+        printf("a é verdadeira\n");
+    }
+
+    if (!b) {
+        printf("b é falso\n");
+    }
+}
