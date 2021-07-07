a8a1335ef0f9519b03122dc1e5830341c81aebf7
nely de faria <nelly.nf54@gmail.com>
2021-07-07 15:02:09 -0300

    Adiciona arquivo do_while.c

---
+++ one-day-code/modulo-03/do_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int j = 0;
+
+    do {
+
+        printf("\n%d", j);
+        j++;
+
+    } while (j <= 10);
+
+    printf("\n");
+
+    return 0;
+}
