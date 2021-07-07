5fc07b28d9ad994743c12919fdf01b283e91edda
nely de faria <nelly.nf54@gmail.com>
2021-07-07 15:01:37 -0300

    Adiciona arquivo while.c

---
+++ one-day-code/modulo-03/while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int j = 0, x = 10;
+
+    while (j < 10) {
+        printf("\n%d", j);
+        j++;
+    }
+
+    while (x >= 0 ) {
+        printf("\n%d", x);
+        x--;
+    }
+
+    printf("\n");
+
+    return 0;
+}
