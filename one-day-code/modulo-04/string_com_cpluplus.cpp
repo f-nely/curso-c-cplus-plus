81d916e499f1bd766b27f12fc8818f2560cd35a6
nely de faria <nelly.nf54@gmail.com>
2021-07-08 12:26:50 -0300

    Adiciona arquivo string_com_cpluplus.cpp

---
+++ one-day-code/modulo-04/string_com_cpluplus.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string palavra;
+
+    printf("Digite uma palavra: \n");
+
+    std::cin >> palavra;
+
+    std::cout << palavra << std::endl;
+
+    return 0;
+}
