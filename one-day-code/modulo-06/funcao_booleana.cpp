a9dd41273b51025b14bbfccd5164e4c8fd95c285
nely de faria <nelly.nf54@gmail.com>
2021-07-10 18:12:42 -0300

    Adiciona arquivo funcao_booleana.cpp

---
+++ one-day-code/modulo-06/funcao_booleana.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool retornaBooleano();
+
+int main()
+{
+    printf("%d\n", retornaBooleano());
+
+    return 0;
+}
+
+bool retornaBooleano()
+{
+    return true;
+}
