ac11de322826967d17ae95952339ced91113b692
nely de faria <nelly.nf54@gmail.com>
2021-07-10 18:12:06 -0300

    Adiciona arquivo funcao_caractere.cpp

---
+++ one-day-code/modulo-06/funcao_caractere.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char retornaLetra();
+
+int main()
+{
+    printf("%c\n", retornaLetra());
+
+    return 0;
+}
+
+char retornaLetra()
+{
+    return 'y';
+}
