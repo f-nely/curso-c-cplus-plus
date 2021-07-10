f8faeecc25d2bc175f62a60541a40b4399229e9f
nely de faria <nelly.nf54@gmail.com>
2021-07-10 18:10:43 -0300

    Adiciona arquivo funcoes_sem_parametro.cpp

---
+++ one-day-code/modulo-06/funcoes_sem_parametro.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void desenhaBorda();
+
+int main()
+{
+    desenhaBorda();
+
+    printf("h1\n");
+
+    desenhaBorda();
+
+    return 0;
+}
+
+void desenhaBorda()
+{
+    printf("|--------------------------------------|\n");
+}
